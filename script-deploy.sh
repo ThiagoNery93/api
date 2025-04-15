4e74193ff0e7b66b31e04de35abb6ba07791e1cf
Thiago Nery <117873254+ThiagoNery93@users.noreply.github.com>
2025-04-15 11:47:43 -0300

    Update script-deploy.sh

--- script-deploy.sh
+++ script-deploy.sh
@@ -7,9 +7,9 @@ mv docker-compose-prod.yaml docker-compose.yaml
 container_ids=$(docker ps -q)
 
 if [ -z"$container_ids" ]; then
-  echo"Não há containers em execução"
+  echo "Não há containers em execução"
 else
-for container_id in $containers_ids; do
+  for container_id in $containers_ids; do
   echo "Parando container: $container_id"
   docker stop $container_id
 done
